de81f26bb5fbb8a8fc471c65e73c97695369d74a
Chris Mungall <cjm@berkeleybop.org>
2017-05-05 17:37:17 -0700

    more rigorous tests

--- tests/test_gafparser.py
+++ tests/test_gafparser.py
@@ -15,12 +15,26 @@ def test_factory():
 def test_skim():
     p = GafParser()
     results = p.skim(open(POMBASE,"r"))
-    print(str(results))
+    for r in results:
+        print(str(r))
+        (s,sn,o) = r
+        assert o.startswith('GO:')
+        assert s.startswith('PomBase:')
     
     
 def test_parse():
+    ont = OntologyFactory().create(ONT)
     p = GafParser()
     results = p.parse(open(POMBASE,"r"))
+    r1 = results[0]
+    assert r1['evidence']['with_support_from'] == 'SGD:S000001583'
+    assert r1['evidence']['has_supporting_reference'] == 'GO_REF:0000024'
+    assert r1['subject']['label'] == 'ypf1'
     for r in results:
         print(str(r))
-    
+        prov = r['provided_by']
+        assert prov == 'PomBase' or prov == 'UniProt'
+        assert r['object']['id'].startswith('GO:')
+        assert r['subject']['id'].startswith('PomBase:')
+        assert r['subject']['taxon']['id'] =='NCBITaxon:4896'
+        
