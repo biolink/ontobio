3ff9ab20b0f27f6230366ddbb3d6f007901c80ee
Chris Mungall <cjm@berkeleybop.org>
2017-05-08 10:40:43 -0700

    testing ttl

---
+++ tests/test_local_ttl.py
@@ -0,0 +1,33 @@
+from ontobio.ontol_factory import OntologyFactory
+import logging
+from ontobio.graph_io import GraphRenderer
+
+PART_OF = 'BFO:0000050'
+
+QUALITY = 'PATO:0000001'
+PLOIDY = 'PATO:0001374'
+EUPLOID = 'PATO:0001393'
+SHAPE = 'PATO:0000052'
+Y_SHAPED = 'PATO:0001201'
+
+def test_local_json_parse():
+    """
+    Load ontology from JSON
+    """
+    factory = OntologyFactory()
+    print("Creating ont")
+    tbox_ontology = factory.create('go')
+    ont = factory.create('tests/resources/gocam-example.ttl', tbox_ontology=tbox_ontology)
+    g = ont.get_graph()
+    nodes = ont.search('%')
+    print("NODES: {}".format(nodes))
+    w = GraphRenderer.create(None)
+    w.write_subgraph(g, nodes)
+    i = 'http://model.geneontology.org/0000000300000001/0000000300000007'
+    ni = g[i]
+    print(str(ni))
+    ['GO:0060070'] == ni['types']
+    nbrs = ont.neighbors(i)
+    print("NEIGHBORS: {}".format(nbrs))
+    
+
