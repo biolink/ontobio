f974cf77659aae3c55bfc0b0717c424df0f8cadd
Eric Douglass <edouglass@lbl.gov>
2017-06-27 14:38:57 -0700

    fix __future__ import

--- ontobio/__init__.py
+++ ontobio/__init__.py
@@ -1,5 +1,6 @@
+from __future__ import absolute_import
+
 __version__ = '0.2.8'
 
-from __future__ import absolute_import
 from .ontol_factory import OntologyFactory
 from .io.ontol_renderers import GraphRenderer
