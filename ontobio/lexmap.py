85696db0fc7fee1d388b45633bb7560b165839d4
Chris Mungall <cjm@berkeleybop.org>
2017-08-14 08:53:38 -0700

    assign best matches

--- ontobio/lexmap.py
+++ ontobio/lexmap.py
@@ -4,7 +4,9 @@ Lexical mapping of ontology classes
 import networkx
 import logging
 import re
-from ontobio.ontol import Synonym
+from ontobio.ontol import Synonym, Ontology
+from collections import defaultdict
+
 
 LABEL_OR_EXACT = 'label_or_exact'
 
@@ -61,18 +63,31 @@ class LexicalMapEngine():
         self.npattern = re.compile('[\W_]+')
         self.exclude_obsolete = True
         self.ontology_pairs = None
+        self.id_to_ontology_map = defaultdict(list)
+        self.merged_ontology = Ontology()
 
+    def index_ontologies(self, onts):
+        logging.info('Indexing: {}'.format(onts))
+        for ont in onts:
+            self.index_ontology(ont)
+        
     def index_ontology(self, ont):
         """
         Adds an ontology to the index
 
         This iterates through all labels and synonyms in the ontology, creating an index
         """
+        self.merged_ontology.merge([ont])
         syns = ont.all_synonyms(include_label=True)
         logging.info("Indexing {} syns in {}".format(len(syns),ont))
         for syn in syns:
             self.index_synonym(syn, ont)
+        for nid in ont.nodes():
+            self.id_to_ontology_map[nid].append(ont)
 
+    def label(self, nid):
+        return self.merged_ontology.label(nid)
+    
     def index_synonym(self, syn, ont):
         """
         Index a synonym
@@ -195,30 +210,61 @@ class LexicalMapEngine():
             return (s1.ontology.id, s2.ontology.id) in self.ontology_pairs
         else:
             return s1.class_id < s2.class_id
+
+    def _blanket(self, nid):
+        nodes = set()
+        for ont in self.id_to_ontology_map[nid]:
+            nodes.update(ont.ancestors(nid))
+            nodes.update(ont.descendants(nid))
+        return list(nodes)
     
-    def score_xrefs_by_semsim(self, xg, ont):
+    def score_xrefs_by_semsim(self, xg, ont=None):
         """
         Given an xref graph (see ref:`get_xref_graph`), this will adjust scores based on
         the semantic similarity of matches.
         """
         for (i,j,d) in xg.edges_iter(data=True):
-            #ont1 = i.ontology
-            #ont2 = j.ontology
-            ancs1 = ont.ancestors(i) + ont.descendants(i)
-            ancs2 = ont.ancestors(j) + ont.descendants(j)
+            #ancs1 = ont.ancestors(i) + ont.descendants(i)
+            #ancs2 = ont.ancestors(j) + ont.descendants(j)
+            ancs1 = self._blanket(i)
+            ancs2 = self._blanket(j)
             s1 = self._sim(xg, ancs1, ancs2)
             s2 = self._sim(xg, ancs2, ancs1)
-            s = s1*s2
+            s = 1 - ((1-s1) * (1-s2))
             logging.debug("Score {} x {} = {} x {} = {} // {}".format(i,j,s1,s2,s, d))
+            xg[i][j][0]['lexscore'] = xg[i][j][0]['score']
+            xg[i][j][0]['simscores'] = (s1,s2)
             xg[i][j][0]['score'] *= s
 
     def _sim(self, xg, ancs1, ancs2):
         xancs1 = set()
         for a in ancs1:
             if a in xg:
+                # TODO: restrict this to neighbors in single ontology
                 xancs1.update(xg.neighbors(a))
+        logging.debug('SIM={}/{} ## {}'.format(len(xancs1.intersection(ancs2)), len(xancs1), xancs1.intersection(ancs2), xancs1))
         return (1+len(xancs1.intersection(ancs2))) / (1+len(xancs1))
-            
+
+
+    def _neighbors_by_ontology(self, xg, nid):
+        xrefmap = defaultdict(list)
+        for x in xg.neighbors(nid):
+            score = xg[nid][x][0]['score']
+            for ont in self.id_to_ontology_map[x]:
+                xrefmap[ont.id].append( (score,x) )
+        return xrefmap
+        
+    def assign_best_matches(self, xg):
+        for i in xg.nodes():
+            xrefmap = self._neighbors_by_ontology(xg, i)
+            for (ontid,pairs) in xrefmap.items():
+                pairs.sort()
+                (bscore,bx) = pairs[0]
+                logging.info("BEST: {}".format(bx))
+                # TODO: refactor for directional
+                xg[bx][i][0]['is_bestA'] = True
+                xg[i][bx][0]['is_bestB'] = True
+        
     def grouped_mappings(self,id):
         """
         return all mappings for a node, grouped by ID prefix
