e11455c3f0783ab5453ca47ea262c196aa116cc3
Chris Mungall <cjm@berkeleybop.org>
2017-08-17 08:19:35 -0700

    include lexmap script in distribution

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setuptools.setup(
         'dev': ['plotly'],
         'test': ['pytest'],
     },
-    scripts=['bin/ogr.py', 'bin/ontobio-assoc.py', 'bin/ontobio-parse-assocs.py']
+    scripts=['bin/ogr.py', 'bin/ontobio-assoc.py', 'bin/ontobio-parse-assocs.py', 'bin/ontobio-lexmap.py']
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
