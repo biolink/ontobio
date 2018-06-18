186a8df54f3d49ca0258a553916eac7f27fcd5a5
Eric Douglass <edouglass@lbl.gov>
2018-06-18 14:11:33 -0700

    removing gorule8 tests

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -15,25 +15,6 @@ def test_result():
     assert qc.result(False, qc.FailMode.HARD) == qc.ResultType.ERROR
     assert qc.result(False, qc.FailMode.SOFT) == qc.ResultType.WARNING
 
-def test_go_rule08():
-    # pass
-    config = assocparser.AssocParserConfig(
-        ontology=ontology
-    )
-
-    a = make_annotation("GO:0006397", "ANY")
-    test_result = qc.GoRule08().test(a, config)
-    assert test_result.result_type == qc.ResultType.PASS
-
-    # fail with manual evidence and do_not_manually_annotate
-    a = make_annotation("GO:0006950", "ANY")
-    test_result = qc.GoRule08().test(a, config)
-    assert test_result.result_type == qc.ResultType.ERROR
-
-    a = make_annotation("GO:0006810", "IEA")
-    test_result = qc.GoRule08().test(a, config)
-    assert test_result.result_type == qc.ResultType.ERROR
-
 def test_go_rule26():
 
     config = assocparser.AssocParserConfig(
@@ -70,8 +51,8 @@ def test_all_rules():
     )
     a = make_annotation("GO:0006397", "ANY")
     test_results = qc.test_go_rules(a, config)
-    assert len(test_results.keys()) == 2
-    assert test_results["GORULE:0000008"].result_type == qc.ResultType.PASS
+    assert len(test_results.keys()) == 1
+    assert test_results["GORULE:0000026"].result_type == qc.ResultType.PASS
 
 
 if __name__ == "__main__":
