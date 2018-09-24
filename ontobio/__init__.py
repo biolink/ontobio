06878f3afc16c8c002c874e686bb9c37e9746f65
Eric Douglass <edouglass@lbl.gov>
2018-09-24 16:52:39 -0700

    Upgrade to v1.4.6

--- ontobio/__init__.py
+++ ontobio/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '1.4.5'
+__version__ = '1.4.6'
 
 from .ontol_factory import OntologyFactory
 from .ontol import Ontology, Synonym, TextDefinition
