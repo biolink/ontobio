78045c100685713f1eadf3cf1f0b23994486670c
Eric Douglass <edouglass@lbl.gov>
2018-10-08 15:09:18 -0700

    pipes not allowed in IDs, fixing some regex

--- tests/test_parse_ids.py
+++ tests/test_parse_ids.py
@@ -14,13 +14,20 @@ def test_no_colon_in_id():
     assert len(parser.report.messages) == 1
     assert parser.report.messages[0]["level"] == assocparser.Report.ERROR
 
+def test_empty_id():
+    parser = gafparser.GafParser()
+    valid = parser._validate_id("", assocparser.SplitLine("", [""] * 17, "taxon:foo"))
+    assert not valid
+    assert len(parser.report.messages) == 1
+    assert parser.report.messages[0]["level"] == assocparser.Report.ERROR
+
 def test_pipe_in_id():
     parser = gafparser.GafParser()
     valid = parser._validate_id("F|OO:123", assocparser.SplitLine("", [""] * 17, "taxon:foo"))
 
-    assert valid
+    assert not valid
     assert len(parser.report.messages) == 1
-    assert parser.report.messages[0]["level"] == assocparser.Report.WARNING
+    assert parser.report.messages[0]["level"] == assocparser.Report.ERROR
 
 def test_bad_character_in_id():
     parser = gafparser.GafParser()
