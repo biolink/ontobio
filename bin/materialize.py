7d58b97e8f99f99f1da25916d198720627c0fdfa
Eric Douglass <edouglass@lbl.gov>
2019-06-06 13:23:30 -0700

    using edges to compute has part

--- bin/materialize.py
+++ bin/materialize.py
@@ -43,20 +43,27 @@ def cli():
 @click.option("--gaf", "-g", type=click.File("r"), required=True)
 def infer(ontology_path, target, gaf):
     ontology_graph = ontology(ontology_path)
-    mf_set = molecular_function_set(ontology_graph)
-    definitions = logical_definitions(ontology_graph)
+    # mf_set = molecular_function_set(ontology_graph)
+    # definitions = logical_definitions(ontology_graph)
 
     writer = GafWriter(file=target)
     assoc_generator = gafparser_generator(ontology_graph, gaf)
+    line_count = 0
     for association in assoc_generator:
         if association["relation"]["id"] != ENABLES:
             continue
             # Skip all non enables annotations
 
-        inferred_associations = materialize_inferences(ontology_graph, definitions, association, mf_set)
-        click.echo("Materialized {} associations".format(len(inferred_associations)))
+        inferred_associations = materialize_inferences(ontology_graph, association)
+        if len(inferred_associations) > 0:
+            click.echo("Materialized {} associations".format(len(inferred_associations)))
+
         for inferred in inferred_associations:
             writer.write_assoc(inferred)
+
+        line_count +=1
+        if line_count % 100 == 0:
+            click.echo("Processed {} lines".format(line_count))
 
 
 
@@ -64,9 +71,9 @@ def ontology(path) -> ontol.Ontology:
     click.echo("Loading ontology from {}...".format(path))
     return OntologyFactory().create(path, ignore_cache=True)
 
-def molecular_function_set(ontology: ontol.Ontology) -> Set[str]:
-    click.echo("Computing Molecular Function ({}) terms...".format(MF))
-    return set(ontology.descendants(MF, "subClassOf", reflexive=True))
+# def molecular_function_set(ontology: ontol.Ontology) -> Set[str]:
+#     click.echo("Computing Molecular Function ({}) terms...".format(MF))
+#     return set(ontology.descendants(MF, "subClassOf", reflexive=True))
 
 def ancestors(term: str, ontology: ontol.Ontology) -> Set[str]:
     click.echo("Computing ancestors for {}".format(term))
@@ -78,21 +85,22 @@ def ancestors(term: str, ontology: ontol.Ontology) -> Set[str]:
     if term not in __ancestors_cache:
         anc = set(ontology.ancestors(term, relations=["subClassOf"]))
         __ancestors_cache[term] = anc
+        click.echo("Found {} (from adding to cache: {} terms added)".format(len(anc), len(__ancestors_cache)))
     else:
         anc = __ancestors_cache[term]
+        click.echo("Found {} (from cache)".format(len(anc)))
 
-    click.echo("Found {}".format(len(anc)))
     return anc
 
-def logical_definitions(ontology: ontol.Ontology) -> Dict:
-    click.echo("Re-normalizing logical definitions...")
-    definition_by_id = dict()
-    for definition in ontology.all_logical_definitions:
-        if definition not in definition_by_id:
-            definition_by_id[definition.class_id] = [definition]
-        else:
-            definition_by_id[definition.class_id].append(definition)
-    return definition_by_id
+# def logical_definitions(ontology: ontol.Ontology) -> Dict:
+#     click.echo("Re-normalizing logical definitions...")
+#     definition_by_id = dict()
+#     for definition in ontology.all_logical_definitions:
+#         if definition not in definition_by_id:
+#             definition_by_id[definition.class_id] = [definition]
+#         else:
+#             definition_by_id[definition.class_id].append(definition)
+#     return definition_by_id
 
 def gafparser_generator(ontology_graph: ontol.Ontology, gaf_file):
     config = assocparser.AssocParserConfig(
@@ -111,25 +119,26 @@ def restrictions_for_term(term, logical_definitions: Dict):
             restrictions[r[0]] = r[1]
     return restrictions
 
-def transorm_relation(mf_annotation, new_mf, ontology_graph):
+def neighbor_by_relation(ontology_graph: ontol.Ontology, term, relation):
+    edges = ontology_graph.get_graph().edges(data=True, nbunch=term)
+    return [n2 for (n1, n2, r) in edges if r["pred"] == relation]
+
+def transform_relation(mf_annotation, new_mf, ontology_graph):
     new_annotation = mf_annotation
     new_annotation["object"]["id"] = new_mf
     # new_annotation["object"]["taxon"] How to taxon?
     return new_annotation
 
-def materialize_inferences(ontology_graph: ontol.Ontology, logical_definitions: Dict, annotation, mf_set):
+def materialize_inferences(ontology_graph: ontol.Ontology, annotation):
     materialized_annotations = [] #(gp, new_mf)
 
     mf = annotation["object"]["id"]
     mf_ancestors = ancestors(mf, ontology_graph)
     for mf_anc in mf_ancestors:
-        restrictions = restrictions_for_term(mf_anc, logical_definitions)
-        click.echo("There are {} restrictions for {}".format(len(restrictions), mf_anc))
-        # Filter restrictions for all has_part and that the filler is a MF
-        has_part_restrictions = { prop: filler for prop, filler in restrictions.items() if (prop == HAS_PART) and (filler in mf_set)}
-        click.echo("Found {} has_part restrictions from the above set".format(len(has_part_restrictions)))
-        for has_part, new_mf in has_part_restrictions.items():
-            new_annotation = transorm_relation(annotation, new_mf, ontology_graph)
+
+        has_part_mfs = neighbor_by_relation(ontology_graph, mf_anc, HAS_PART)
+        for new_mf in has_part_mfs:
+            new_annotation = transform_relation(annotation, new_mf, ontology_graph)
             materialized_annotations.append(new_annotation)
 
     return materialized_annotations
