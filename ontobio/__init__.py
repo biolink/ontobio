f189a2ba3efb42170c78a7258f23a8ab282166d2
Eric Douglass <edouglass@gmail.com>
2020-03-17 09:13:50 -0700

    Upgrade to v1.17.0

--- ontobio/__init__.py
+++ ontobio/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '1.16.2'
+__version__ = '1.17.0'
 
 from .ontol_factory import OntologyFactory
 from .ontol import Ontology, Synonym, TextDefinition
