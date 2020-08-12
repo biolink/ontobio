8fadb065cd333a8c44d7f7eb4530fe3a25ac2271
Eric Douglass <edouglass@lbl.gov>
2020-08-12 13:24:28 -0700

    setting pre-release version to 2.0.0-pre0

--- ontobio/__init__.py
+++ ontobio/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '1.19.3'
+__version__ = '2.0.0-pre0'
 
 from .ontol_factory import OntologyFactory
 from .ontol import Ontology, Synonym, TextDefinition
