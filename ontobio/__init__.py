7e243abd38da99d7f0aef94f64086b6ae9709d79
Eric Douglass <edouglass@gmail.com>
2020-08-25 12:26:47 -0700

    update branch to 2.0.0-pre6

--- ontobio/__init__.py
+++ ontobio/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '2.0.0-pre5'
+__version__ = '2.0.0-pre6'
 
 from .ontol_factory import OntologyFactory
 from .ontol import Ontology, Synonym, TextDefinition
