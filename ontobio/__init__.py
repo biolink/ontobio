cba646085829cb0879d702c938c2588a0b5d3ff7
Eric Douglass <edouglass@gmail.com>
2020-09-17 18:16:39 -0700

    updated to 2.0.0-pre9

--- ontobio/__init__.py
+++ ontobio/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '2.0.0-pre8'
+__version__ = '2.0.0-pre9'
 
 from .ontol_factory import OntologyFactory
 from .ontol import Ontology, Synonym, TextDefinition
