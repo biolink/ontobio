ad7f4116301b5a65c6c810124eefcd5fc6bb220b
Eric Douglass <edouglass@gmail.com>
2020-10-27 17:50:15 -0700

    curies should not parse with spaces in them, tests for new validation

--- tests/test_gpad_parser.py
+++ tests/test_gpad_parser.py
@@ -79,6 +79,16 @@ def test_parse_2_0():
         ConjunctiveSet([Curie("PR", "Q8CHK4")])
     ]
 
+    # Interacting taxon - this example should fail
+    vals[7] = "Staphylococcus aureus ; NCBITaxon:1280"
+    result = to_association(list(vals), report=report, version=version)
+    assert len([m for m in result.report.messages if m["level"] == "ERROR"]) > 0
+    assert len(result.associations) == 0
+    # Now test valid interacting taxon value
+    vals[7] = "NCBITaxon:1280"
+    result = to_association(list(vals), report=report, version=version)
+    assert result.associations[0].interacting_taxon == Curie("NCBITaxon", "1280")
+
     # Confirm non-"MGI:MGI:" IDs will parse
     vals[0] = "WB:WBGene00001189"
     result = to_association(list(vals), report=report, version=version)
