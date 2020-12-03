5f5291605b48411da9d68a3fce3d344454620481
Eric Douglass <dougli1sqrd+git@gmail.com>
2020-12-03 10:37:23 -0800

    unlocking some dependencies

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
 
     # Dependencies
     install_requires=[
-        'networkx==2.2',
+        'networkx',
         'jsobject',
         'pyyaml',
         'pysolr',
@@ -53,7 +53,7 @@ setuptools.setup(
         'prefixcommons',
         'marshmallow==3.0.0b11',
         'scipy',
-        'pandas==1.0.5',
+        'pandas',
         'click',
         'yamldown',
         'dataclasses',
