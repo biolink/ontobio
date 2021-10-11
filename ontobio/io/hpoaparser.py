498588591b04e220bf0c2083e4e4f8a2c5ab7ecf
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-10-11 13:20:54 -0700

    talking to Kent, leave HPOA out of this change.

--- ontobio/io/hpoaparser.py
+++ ontobio/io/hpoaparser.py
@@ -174,9 +174,8 @@ class HpoaParser(AssocParser):
                 relation = None
 
         # With/From
-        # withfroms = self.validate_pipe_separated_ids(withfrom, split_line, empty_allowed=True, extra_delims=",")
+        withfroms = self.validate_pipe_separated_ids(withfrom, split_line, empty_allowed=True, extra_delims=",")
 
-        withfroms = self._unroll_withfrom_and_replair_obsoletes(split_line, 'gpad')
         if withfroms is None:
             # Reporting occurs in above function call
             return assocparser.ParseResult(line, [], True)
