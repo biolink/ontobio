ef749653a7c2ed1c3c846040dcf969a1d740e020
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-11-03 12:18:38 -0700

    progress on simplifying reporting

--- ontobio/io/gpaddiffer.py
+++ ontobio/io/gpaddiffer.py
@@ -12,66 +12,64 @@ from ontobio.io.assocparser import Report
 @click.option("--file1", "-file1", type=click.Path(), required=True)
 @click.option("--file2", "-file2", type=click.Path(), required=True)
 @click.option("--output", "-o", type=click.STRING, required=True)
-@click.option("--count_by", "-cb", type=click.STRING, multiple=True, required=False)
+@click.option("--group_by_column", "-gb", type=click.STRING, multiple=True, required=False)
 @click.option("--file_type", "-file_type", type=click.STRING, required=True)
-@click.option("--exclude_details", "-ed", type=click.BOOL, default=True, required=False)
-def compare_files(file1, file2, output, count_by, exclude_details, file_type):
+def compare_files(file1, file2, output, group_by_column, file_type):
     # decide which parser to instantiate, GAF or GPAD
     pd.set_option('display.max_rows', 35000)
     df_file1, df_file2, assocs1, assocs2 = get_parser(file1, file2, file_type)
 
-    # if count_by is passed in, generate a report of annotations grouped by count_by
-    # count_by is a list.
-    generate_group_report(df_file1, df_file2, count_by, file1, file2, output)
+    # group_by is a list of strings exactly matching column names.
+    generate_group_report(df_file1, df_file2, group_by_column, file1, file2, output)
 
-    # if exclude_details is False, try to compare files line by line and find matches.
-    compare_associations(assocs1, assocs2, exclude_details, output)
-    generate_count_report(df_file1, df_file2, count_by, file1, file2, output)
+    compare_associations(assocs1, assocs2, output)
+    generate_count_report(df_file1, df_file2, group_by_column, file1, file2, output)
 
 
-def generate_count_report(df_file1, df_file2, count_by, file1, file2, output):
-    count_by_report_file = open(output + "_counts_per_column_report", "w")
-    file1_groups = group_by(df_file1, count_by, file1)
-    file2_groups = group_by(df_file2, count_by, file2)
-    print(("Counts By Column: %s file: " % "\n" + file1) + "\n" + str(file1_groups.get("counts_per_column")) + "\n")
-    count_by_report_file.write(str(file1_groups.get("counts_per_column")))
-    print(("Counts By Column: %s file: " % "\n" + file2) + "\n" + str(file2_groups.get("counts_per_column")) + "\n")
-    count_by_report_file.write(str(file2_groups.get("counts_per_column")))
+def generate_count_report(df_file1, df_file2, to_group_by, file1, file2, output):
+    file1_groups, counts_frame1, grouped_frame1 = group_by(df_file1, to_group_by, file1)
+    file2_groups, counts_frame2, grouped_frame2 = group_by(df_file2, to_group_by, file2)
+
+    merged_frame = pd.concat([counts_frame1, counts_frame2], axis=1)
+    merged_frame.to_csv(output + "_counts_per_column_report", sep='\t')
+    print(merged_frame)
 
 
-def generate_group_report(df_file1, df_file2, count_by, file1, file2, output):
-    if len(count_by) > 0:
-        group_by_report_file = open(output + "_group_by_report", "w")
-        file1_groups = group_by(df_file1, count_by, file1)
-        file2_groups = group_by(df_file2, count_by, file2)
-        print(file1_groups.get("counts_by_column"))
+def generate_group_report(df_file1, df_file2, group_by_column, file1, file2, output):
+    if len(group_by_column) > 0:
 
         s = "\n\n## GROUP BY SUMMARY \n\n"
         s += "This report generated on {}\n\n".format(datetime.date.today())
-        s += "  * Group By Columns: " + str(count_by) + "\n"
+        s += "  * Group By Columns: " + str(group_by_column) + "\n"
         s += "  * Compared Files: " + file1 + ", " + file2 + "\n"
-
         print(s)
-        print(("Counts By Column: %s file: " % "\n" + file1) + "\n" + str(file1_groups.get("counts_per_column")) + "\n")
-        print(("Counts By Column: %s file: " % "\n" + file2) + "\n" + str(file2_groups.get("counts_per_column")) + "\n")
 
-        for grouped_item in file1_groups['grouped_reports']:
-            print(file1_groups['filename'])
-            group_by_report_file.write(file1_groups['filename'])
-            print(grouped_item)
-            group_by_report_file.write(str(grouped_item))
+        for group in group_by_column:
+            file1_groups, file1_counts_df, grouped_frame1 = group_by(df_file1, group, file1)
+            file2_groups, file2_counts_df, grouped_frame2 = group_by(df_file2, group, file2)
+            merged_group_frame = pd.concat([grouped_frame1, grouped_frame2], axis=1)
+
+            merged_group_frame.to_csv(output + "_" + group + "_counts_per_column_report", sep='\t')
             print("\n")
+            print(merged_group_frame)
 
-        for grouped_item in file2_groups['grouped_reports']:
-            print(file2_groups['filename'])
-            group_by_report_file.write(file2_groups['filename'])
-            print(grouped_item)
-            group_by_report_file.write(str(grouped_item))
+        # for grouped_item in file1_groups['grouped_reports']:
+        #     print(file1_groups['filename'])
+        #     group_by_report_file.write(file1_groups['filename'])
+        #     print(grouped_item)
+        #     group_by_report_file.write(str(grouped_item))
+        #     print("\n")
+        #
+        # for grouped_item in file2_groups['grouped_reports']:
+        #     print(file2_groups['filename'])
+        #     group_by_report_file.write(file2_groups['filename'])
+        #     print(grouped_item)
+        #     group_by_report_file.write(str(grouped_item))
+        #
+        # group_by_report_file.close()
 
-        group_by_report_file.close()
 
-
-def compare_associations(assocs1, assocs2, exclude_details, output):
+def compare_associations(assocs1, assocs2,  output):
     compare_report_file = open(output + "_compare_report", "w")
     processed_lines = 0
     exact_matches = 0
@@ -81,45 +79,43 @@ def compare_associations(assocs1, assocs2, exclude_details, output):
     for association in assocs1:
         max_match_score = 0
         processed_lines = processed_lines + 1
-        if not exclude_details:
-            for target in assocs2:
-                match_score = 0
-                if association.negated != target.negated:
-                    continue
-                if association.subject.id == target.subject.id and association.object.id == target.object.id:
-                    match_score = 1
-                    if sorted(str(q).upper() for q in association.qualifiers) == \
-                            sorted(str(q).upper() for q in target.qualifiers):
-                        match_score = 2
-                        if association.evidence.type == target.evidence.type:
-                            match_score = 3
-                            if sorted(str(w).upper() for w in association.evidence.with_support_from) == \
-                                    sorted(str(w).upper() for w in target.evidence.with_support_from):
-                                match_score = 4
-                                if sorted(
-                                        str(r).upper() for r in association.evidence.has_supporting_reference) == \
-                                        sorted(str(r).upper() for r in target.evidence.has_supporting_reference):
-                                    match_score = 5
-                if match_score > max_match_score:
-                    max_match_score = match_score
-            if max_match_score > 4:
-                exact_matches = exact_matches + 1
-            elif 1 > max_match_score < 5:
-                close_matches = close_matches + 1
-                report.add_association(association)
-                report.n_lines = report.n_lines + 1
-                report.warning(association.source_line, qc.ResultType.WARNING,
-                               "line from file1 only has CLOSE match in file2", "")
-            else:
-                report.add_association(association)
-                report.n_lines = report.n_lines + 1
-                report.error(association.source_line, qc.ResultType.ERROR,
-                             "line from file1 has NO match in file2", "")
-    if not exclude_details:
-        md_report = markdown_report(report, exact_matches, close_matches, processed_lines)
-        print(md_report)
-        compare_report_file.write(md_report)
-        compare_report_file.close()
+        for target in assocs2:
+            match_score = 0
+            if association.negated != target.negated:
+                continue
+            if association.subject.id == target.subject.id and association.object.id == target.object.id:
+                match_score = 1
+                if sorted(str(q).upper() for q in association.qualifiers) == \
+                        sorted(str(q).upper() for q in target.qualifiers):
+                    match_score = 2
+                    if association.evidence.type == target.evidence.type:
+                        match_score = 3
+                        if sorted(str(w).upper() for w in association.evidence.with_support_from) == \
+                                sorted(str(w).upper() for w in target.evidence.with_support_from):
+                            match_score = 4
+                            if sorted(
+                                    str(r).upper() for r in association.evidence.has_supporting_reference) == \
+                                    sorted(str(r).upper() for r in target.evidence.has_supporting_reference):
+                                match_score = 5
+            if match_score > max_match_score:
+                max_match_score = match_score
+        if max_match_score > 4:
+            exact_matches = exact_matches + 1
+        elif 1 > max_match_score < 5:
+            close_matches = close_matches + 1
+            report.add_association(association)
+            report.n_lines = report.n_lines + 1
+            report.warning(association.source_line, qc.ResultType.WARNING,
+                           "line from file1 only has CLOSE match in file2", "")
+        else:
+            report.add_association(association)
+            report.n_lines = report.n_lines + 1
+            report.error(association.source_line, qc.ResultType.ERROR,
+                         "line from file1 has NO match in file2", "")
+    md_report = markdown_report(report, exact_matches, close_matches, processed_lines)
+    print(md_report)
+    compare_report_file.write(md_report)
+    compare_report_file.close()
 
 
 def markdown_report(report, exact_matches, close_matches, processed_lines):
@@ -229,19 +225,11 @@ def read_gpad_csv(filename):
     return data_frame
 
 
-def group_by(data_frame, count_by, file):
-    stats = {}
-    grouped_reports = []
-    stats['filename'] = file
-    stats['total_rows'] = data_frame.shape[0]
-    stats['counts_per_column'] = data_frame.nunique()
-    if len(count_by) > 0:
-        for grouper in count_by:
-            stats['grouper'] = grouper
-            grouped_reports.append(data_frame.groupby(grouper)[grouper].count())
-        stats['grouped_reports'] = grouped_reports
-
-    return stats
+def group_by(data_frame, group, file):
+    stats = {'filename': file, 'total_rows': data_frame.shape[0]}
+    count_frame = data_frame.nunique().to_frame(file)
+    grouped_frame = data_frame.groupby(group)[group].count().to_frame()
+    return stats, count_frame, grouped_frame
 
 
 if __name__ == '__main__':
