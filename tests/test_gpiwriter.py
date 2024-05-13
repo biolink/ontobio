f03b059331e3e3f0b69282dc7e04f80483935452
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-05-13 16:01:17 -0700

    validate now produces clean gpad with additional data from noctua, GPI now has a flag to switch between GPAD/GPI 2.0 and 1.2, plus tests

--- tests/test_gpiwriter.py
+++ tests/test_gpiwriter.py
@@ -3,29 +3,63 @@ import io
 from ontobio.io import entitywriter, gafgpibridge
 
 
-def test_header_newline():
+def test_gpi_2_0_writer():
     gpi_obj = {
         'id': "MGI:MGI:1918911",
         'label': "0610005C13Rik",  # db_object_symbol,
         'full_name': "RIKEN cDNA 0610005C13 gene",  # db_object_name,
         'synonyms': [],
-        'type': ["gene"],  # db_object_type,
-        'parents': "",  # GAF does not have this field, but it's optional in GPI
-        'xrefs': "",  # GAF does not have this field, but it's optional in GPI
-        'taxon': {
-            'id': "NCBITaxon:10090"
-        }
+        'type': ["SO:0000000"],  # db_object_type,
+        'taxon': {"id": "NCBITaxon:10090"},
+        'encoded_by': "", # encoded_by
+        'parents': "",
+        'protein_containing_complex_members': "", # protein_containing_complex_members
+        'xrefs': "",
+        'properties': ""
+
     }
+
     entity = gafgpibridge.Entity(gpi_obj)
 
     out = io.StringIO()
-    gpiwriter = entitywriter.GpiWriter(file=out)
-    gpiwriter.write_entity(entity)
+    gpiwriter20 = entitywriter.GpiWriter(file=out, version="2.0")
+    gpiwriter20.write_entity(entity)
+    outlines = out.getvalue().split("\n")
+
+    expected_lines = [
+        "!gpi-version: 2.0",
+        "MGI:MGI:1918911\t0610005C13Rik\tRIKEN cDNA 0610005C13 gene\t\tSO:0000000\ttaxon:10090\t\t\t\t\t",
+        ""
+    ]
+    assert expected_lines == outlines
+
+
+def test_gpi_1_2_writer():
+    gpi_obj = {
+        'id': "MGI:MGI:1918911",
+        'label': "0610005C13Rik",  # db_object_symbol,
+        'full_name': "RIKEN cDNA 0610005C13 gene",  # db_object_name,
+        'synonyms': [],
+        'type': ["SO:0000000"],  # db_object_type,
+        'taxon': {"id": "NCBITaxon:10090"},
+        'encoded_by': "",  # encoded_by
+        'parents': "",
+        'protein_containing_complex_members': "",  # protein_containing_complex_members
+        'xrefs': "",
+        'properties': ""
+
+    }
+
+    entity = gafgpibridge.Entity(gpi_obj)
+
+    out = io.StringIO()
+    gpiwriter20 = entitywriter.GpiWriter(file=out, version="1.2")
+    gpiwriter20.write_entity(entity)
     outlines = out.getvalue().split("\n")
 
     expected_lines = [
         "!gpi-version: 1.2",
-        "MGI\tMGI:1918911\t0610005C13Rik\tRIKEN cDNA 0610005C13 gene\t\tgene\ttaxon:10090\t\t\t",
+        "MGI\tMGI:1918911\t0610005C13Rik\tRIKEN cDNA 0610005C13 gene\t\tSO:0000000\ttaxon:10090\t\t\t",
         ""
     ]
     assert expected_lines == outlines
