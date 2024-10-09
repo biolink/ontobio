bcedebc7b3edcdf110d534ba37412e32ba18a581
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-10-09 11:27:41 -0700

    fix tests, albeit a fairly useless test if the relations.py dict needs to change again.

--- tests/test_gpad_writer.py
+++ tests/test_gpad_writer.py
@@ -1,13 +1,18 @@
 import io
 from pathlib import Path
-
+from pprint import pprint
 from ontobio.io import assocparser
 from ontobio.io import gafparser
 from ontobio.io import gpadparser
 from ontobio.io import assocwriter
 
 
-def test_gpad_relation_writing():
+def test_gpad_extension_relation_writing():
+    """
+    Test the writing of a GPAD file with extension relations using relations.py dict.
+
+    https://github.com/geneontology/pipeline/issues/397
+    """
     out = io.StringIO()
     parser = gpadparser.GpadParser()
     parser.config = assocparser.AssocParserConfig(
@@ -19,8 +24,14 @@ def test_gpad_relation_writing():
 
     # single_relation.gpad is a one line GPAD file in 1.2 format
     gpad_1_2_input_path = base_path / "single_relation.gpad"
+
     for assoc in parser.association_generator(skipheader=True, file=open(gpad_1_2_input_path)):
         writer.write_assoc(assoc)
+        for object_extension_unit in assoc.object_extensions:
+            for element in object_extension_unit.elements:
+                if str(element.relation).startswith("RO"):
+                    pprint(element)
+                    assert str(element.relation) != "RO:0002449"
 
     outlines = out.getvalue().split("\n")
 
@@ -28,8 +39,6 @@ def test_gpad_relation_writing():
     assert outlines[1] == "!generated-by: GOC"
     assert outlines[2].startswith("!date-generated:")
     assert outlines[3] == "UniProtKB:P46934\t\tRO:0002327\tGO:0004842\tPMID:17996703\tECO:0000315\t\t\t2018-05-01\tWB\tBFO:0000050(GO:0006511),BFO:0000050(GO:0006974),BFO:0000066(GO:0005634),RO:0002233(UniProtKB:P24928),RO:0002630(GO:0001055)\tmodel-state=production|noctua-model-id=gomodel:5323da1800000002|contributor=https://orcid.org/0000-0002-1706-4196"
-
-
 
 
 def test_gpad_iba_writing():
